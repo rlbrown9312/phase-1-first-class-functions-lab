35f5b52756eb5dd7abc61acff4b6af0851acc737
Richard <richard.brown0807@gmail.com>
2022-11-19 15:47:00 -0600

    commit tree ed24d89655d862b1c1ee612c1fff68f8d9393ee0

--- index.js
+++ index.js
@@ -4,52 +4,56 @@ function returnFirstTwoDrivers(drivers) {
 }
 
 
-function returnLastTwoDrivers(drivers) {
+const returnLastTwoDrivers = (drivers) => {
     const lastDrivers = drivers.slice(2); 
         return lastDrivers;
 }
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
-// function selectingDrivers() {
-//     const a1 = returnFirstTwoDrivers; 
-//     console.log(a1);
-//     console.log(a1())
-//     console.log(a1()[0])
-//     const combine = [a1, returnLastTwoDrivers]
-//         return combine;
+
+const createFareMultiplier = function(int) {
+  
+    const totalPrice = function(fare) {
+      return int * fare  
+    }
+  
+    return totalPrice
+}
+
+
+const fareDoubler = fare => fare * 2
+
+
+const fareTripler = fare => fare * 3
+
+
+const selectDifferentDrivers = (arrayOfDrivers, inputFunc) => {
+  return inputFunc(arrayOfDrivers)
+}
+
+
+
+
+    //Code that works, but I don't favor it
+// function createFareMultiplier() {
+//     return quintifier 
+//  }
+   
+// function quintifier(num) {
+//     return num * 5;
 // }
 
+// //A function that doubles fares
+// function fareDoubler(num) {
+//     return num * 2;
+// }
 
-//Next two functions are combined and returned to create a Higher function
-function createFareMultiplier() {
-    return quintifier 
- }
-   
-function quintifier(num) {
-    return num * 5;
-}
+// //A function that triples fares
+// function fareTripler(num) {
+//     return num * 3;
+// }
 
-//console.log(createFareMultiplier()(7));
-
-//A function that doubles fares
-function fareDoubler(num) {
-    return num * 2;
-}
-
-//A function that triples fares
-function fareTripler(num) {
-    return num * 3;
-}
-
-function selectDifferentDrivers(arrayOfDrivers, inputFunction) {    
-    return inputFunction(arrayOfDrivers);
-}
-
-
-
-function add(x){
-    return x + 1;
-}
-
-console.log(add());
+// function selectDifferentDrivers(arrayOfDrivers, inputFunction) {    
+//     return inputFunction(arrayOfDrivers);
+// }
